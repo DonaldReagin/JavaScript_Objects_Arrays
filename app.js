7253129c9fdcd7967a2b6c1a27586ee28e1e26d7
Donald Reagin <don.rage616@gmail.com>
2021-06-23 14:27:59 -0400

    Completed exercises

--- app.js
+++ app.js
@@ -3,7 +3,26 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
+const numbers = [2, 22, 12 ,17, 18, 39, 129];
+const arraySum = (numbers) => {
+    let sum = 0;
+    numbers.forEach((num) => {
+        sum += num;
+    });
+    return sum;
+};
+
+console.log(arraySum(numbers));
+
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
 
+const book = {};
+book.title = "Everything's Eventual by Stephen King";
+book.pages = 459;
+book.readCount = 1;
+book.info = () => {
+    return `${book.title}, ${book.pages} pages, Read ${book.readCount} times.`;
+};
 
+console.log(book.info());
